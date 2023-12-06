fe9484d1ee3783c4504add7f2139944b9d647adc
Evgeniia11 <153060149+Evgeniia11@users.noreply.github.com>
2023-12-07 01:10:05 +0600

    creating and implementing CardAcceptor and CustomException

---
+++ src/receiver/CardAcceptor.java
@@ -0,0 +1,93 @@
+package receiver;
+
+import exceptions.CustomException;
+
+import java.util.Scanner;
+
+public class CardAcceptor implements MoneyReceiver {
+    private int amount;
+    private String numberOfCard;
+    private String password;
+
+    @Override
+    public int getAmount() {
+        return amount;
+    }
+
+    @Override
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public void addMoney(){
+        boolean isRun = true;
+        while (isRun) {
+            try {
+                if (numberOfCard == null || numberOfCard.isEmpty() || numberOfCard.isBlank()) {
+                    String number = inputFromConsole("Введите номер карты (16 цифр).");
+                    if (number.length() != 16) {
+                        throw new CustomException("Введено не верное количество символов, повторите еще раз.");
+                    }
+                    numberOfCard = number;
+                } else if (password == null || password.isEmpty() || password.isBlank()){
+                    String onTimePassword = checkPassword("Введите пароль.");
+                    if (onTimePassword.length() != 4) {
+                        throw new CustomException("Введен не верный пароль.");
+                }
+                    password = onTimePassword;
+                } else {
+                   amount = parseInt("Введите сумму пополнения.");
+                   isRun = false;
+                }
+            } catch (NumberFormatException | CustomException e) {
+                e.printStackTrace();
+                System.err.println("Введены неверные данные.");
+            }
+        }
+    }
+
+    protected String inputFromConsole(String text) {
+        while (true) {
+            System.out.print(text);
+            String str = new Scanner(System.in).nextLine();
+            if (str.isEmpty() || str.isBlank()) {
+                System.err.println("Не корректно введены данные.");
+            } else {
+                return str;
+            }
+        }
+    }
+
+    protected String checkPassword(String text) {
+        while (true) {
+            String resultText = inputFromConsole(text);
+            try {
+                int result = Integer.parseInt(resultText);
+                if (result < 0) {
+                    throw new CustomException("Пароль должен содержать только цифры.");
+                }
+                return resultText;
+            } catch (NumberFormatException | CustomException e) {
+                e.printStackTrace();
+                System.err.println("Введены неверные данные.");
+            }
+        }
+    }
+
+    protected int parseInt(String text) {
+        while (true) {
+            String resultText =inputFromConsole(text);
+            try {
+                int result = Integer.parseInt(resultText);
+                if (result < 0) {
+                    throw new CustomException("Введенное значение не должно быть меньше 0.");
+                }
+                return result;
+            } catch (NumberFormatException | CustomException e) {
+                e.printStackTrace();
+                System.err.println("Введены неверные данные.");
+            }
+        }
+    }
+}
